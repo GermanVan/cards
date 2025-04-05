53ec094498ad7eb088ad501b1267a516c260e3c5
GermanVan <supergera95@gmail.com>
2025-04-05 21:43:56 +0300

    Chapter 16

---
+++ Cards/CardsPlayground.playground/Contents.swift
@@ -0,0 +1,93 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        setupViews()
+    }
+    
+    private func setupViews() {
+        self.view = getRootView()
+        let redView = getRedView()
+        let greenView = getGreenView()
+        let whiteView = getWhiteView()
+        let pinkView = getPinkView()
+        set(view: greenView, toCenterOfView: redView)
+        whiteView.center = greenView.center
+        redView.transform = CGAffineTransform(rotationAngle: .pi/3)
+        
+        self.view.addSubview(redView)
+        redView.addSubview(greenView)
+        redView.addSubview(whiteView)
+        self.view.addSubview(pinkView)
+    }
+    
+    private func getRootView() -> UIView {
+        let view = UIView()
+        view.backgroundColor = .gray
+        return view
+    }
+    
+    private func getRedView() -> UIView {
+        let viewFrame = CGRect(x: 50, y: 50, width: 200, height: 200)
+        let view = UIView(frame: viewFrame)
+        view.backgroundColor = .red
+        view.clipsToBounds = true
+        return view
+    }
+    
+    private func getGreenView() -> UIView {
+        let viewFrame = CGRect(x: 10, y: 10, width: 180, height: 180)
+        let view = UIView(frame: viewFrame)
+        view.backgroundColor = .green
+        return view
+    }
+    
+    private func getWhiteView() -> UIView {
+        let viewFrame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        let view = UIView(frame: viewFrame)
+        view.backgroundColor = .white
+        return view
+    }
+    
+    private func getPinkView() -> UIView {
+        let viewFrame = CGRect(x: 50, y: 300, width: 100, height: 100)
+        let view = UIView(frame: viewFrame)
+        view.backgroundColor = .systemPink
+        view.layer.borderWidth = 5
+        view.layer.borderColor = UIColor.yellow.cgColor
+        view.layer.cornerRadius = 10
+        view.layer.shadowOpacity = 1
+        view.layer.shadowRadius = 20
+        view.layer.shadowOffset = CGSize(width: 10, height: 20)
+        view.layer.shadowColor = UIColor.white.cgColor
+        view.layer.opacity = 0.7
+        
+        let layer = CALayer()
+        layer.backgroundColor = UIColor.black.cgColor
+        layer.frame = CGRect(x: 10, y: 10, width: 20, height: 20)
+        layer.cornerRadius = 10
+        view.layer.addSublayer(layer)
+        
+        print(view.frame)
+                
+        return view
+    }
+    
+    private func set(view moveView: UIView, toCenterOfView baseView: UIView) {
+        let moveViewWidth = moveView.frame.width
+        let moveViewHeight = moveView.frame.height
+        let baseViewWidth = baseView.bounds.width
+        let baseViewHeight = baseView.bounds.height
+        let newXCoordinate = (baseViewWidth - moveViewWidth) / 2
+        let newYCoordinate = (baseViewHeight - moveViewHeight) / 2
+        moveView.frame.origin = CGPoint(x: newXCoordinate, y: newYCoordinate)
+        moveView.center = CGPoint(x: baseView.bounds.midX, y: baseView.bounds.midY)
+    }
+
+   
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
